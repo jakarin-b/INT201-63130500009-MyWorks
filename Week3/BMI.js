fd50388a441e39b3f74c1545280d1e8913b7ca2d
jakarin-b <jakarin.b@mail.kmutt.ac.th>
2021-08-24 12:39:47 +0700

    pratice week3

---
+++ Week3/BMI.js
@@ -0,0 +1,18 @@
+//attibuilts 
+let pounds;
+let inchs;
+
+function BmiCalculateByPoundsAndInch(pounds,inchs){
+    //convert pounds to kg
+    let kg = pounds*0.453592;
+    //convert inch to meter 
+    let meter = inchs*0.0254;
+    //calculate bmi by use kg and meter (p.s!! use Math.pow(base,exponent) 
+    let BMI = kg/Math.pow(meter,2);
+
+    //return BMI for ans.
+    return BMI;
+
+}
+//use the function for result
+console.log(BmiCalculateByPoundsAndInch(36.5,39));
---
+++ Week3/Zodiac.js
@@ -0,0 +1,37 @@
+let zodiac = [`monkey`,`rooster`,`dog`,`pig`,`rat`,`ox`,`tiger`,`rabbit`,
+                `dragon`,`snake`,`horse`,`sheep`];
+
+let month = 1900;
+let zodiacType = month%12;
+
+console.log(`zodiac is : ${zodiac[zodiacType]}`);
+
+    // switch(month%12){
+
+    // case 0: console.log(`zodiac is : ${zodiac[0]}`);
+    //     break;
+    // case 1: console.log(`zodiac is : ${zodiac[1]}`);
+    //     break;
+    // case 2: console.log(`zodiac is : ${zodiac[2]}`);
+    //     break;
+    // case 3: console.log(`zodiac is : ${zodiac[3]}`);
+    //     break;
+    // case 4: console.log(`zodiac is : ${zodiac[4]}`);
+    //     break;
+    // case 5: console.log(`zodiac is : ${zodiac[5]}`);
+    //     break;
+    // case 6: console.log(`zodiac is : ${zodiac[6]}`);
+    //     break;
+    // case 7: console.log(`zodiac is : ${zodiac[7]}`);
+    //     break;
+    // case 8: console.log(`zodiac is : ${zodiac[8]}`);
+    //     break;
+    // case 9: console.log(`zodiac is : ${zodiac[9]}`);
+    //     break;
+    // case 10: console.log(`zodiac is : ${zodiac[10]}`);
+    //     break;
+    // case 11: console.log(`zodiac is : ${zodiac[11]}`);
+    //     break;
+    
+// }
+
